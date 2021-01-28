7ceac318838c95be7b30ed2867d90211ee9ca241
deokgoo <kkddgg1001@gmail.com>
2021-01-28 21:41:01 +0900

    oop/abstract

--- oop/coffeeMachine_oop.ts
+++ oop/coffeeMachine_oop.ts
@@ -1,36 +1,90 @@
-() => {
-  type Coffee = {
+{
+  type CoffeeCup = {
     shot: number;
     isIncludeMilk: boolean;
+    color: 'red' | 'black' | 'white';
   }
-  class CoffeeMachine {
-    private coffeeBean: number;
-    private static BEAN_PER_SHOT: number = 8;
-    constructor(coffeeBean: number) {
-      this.coffeeBean = coffeeBean;
+  type Order = {
+    shots: number;
+    milk?: boolean;
+  }
+
+  interface CoffeeMaker {
+    makeCoffee(order: Order): CoffeeCup;
+  }
+
+  interface CommercialCoffeeMaker {
+    makeCoffee(order: Order): CoffeeCup;
+    fillCoffeeBeans(bean: number): void;
+    clean(): void;
+  }
+
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
+    private static _BEAN_PER_SHOT: number = 8;
+    private _coffeeBean: number= 0;
+
+    private constructor(initBean: number) {
+      this.fillCoffeeBeans(initBean);
     }
-    getBean(): number {
-      return this.coffeeBean;
+    static create(initBean: number): CoffeeMachine {
+      return new CoffeeMachine(initBean);
     }
-    addBean(coffeeBean: number) {
-      this.coffeeBean += coffeeBean;
+
+    get bean(): number {
+      return this._coffeeBean;
     }
-    consumeBean(coffeeBean: number) {
-      this.coffeeBean -= coffeeBean;
+
+    fillCoffeeBeans(coffeeBean: number): number {
+      if(coffeeBean < 0) throw new Error('value for bean should be greather than 0');
+      this._coffeeBean += coffeeBean;
+      return this.bean;
     }
-    createCoffee({shot, milk = false}: {shot: number, milk: boolean}): Coffee {
-      if(this.BEAN_PER_SHOT*shot > this.coffeeBean) throw new Error('not enough bean');
-      this.consumeBean(this.BEAN_PER_SHOT*shot);
+
+    grindBeans(shots: number) {
+      const needBean = CoffeeMachine._BEAN_PER_SHOT * shots;
+      console.log('✅ check the bean...');
+      if(needBean > this._coffeeBean) throw new Error('not enough coffee bean ');
+      console.log('🧵 gridBeans....');
+      this._coffeeBean -= needBean;
+    }
+
+    preheat() {
+      console.log('🔥 preheating... ');
+    }
+
+    extract({shots, milk=false}: Order): CoffeeCup {
       return {
-        shot,
+        shot: shots,
         isIncludeMilk: milk,
+        color: 'white',
       }
     }
-    static createCoffeeMachine(initBean: number): CoffeeMachine {
-      return new CoffeeMachine(initBean);
+
+    clean() {
+      console.log('cleaning the machine...🧽')
+    }
+
+    makeCoffee({shots, milk}: Order): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract({shots, milk});
     }
   }
 
-  const maker = new CoffeeMachine(123);
-  const maker2 = CoffeeMachine.createCoffeeMachine(123);
+  const coffeeMachine1: CoffeeMachine = CoffeeMachine.create(150);
+  const order1 = coffeeMachine1.makeCoffee({shots: 2, milk: false});
+  console.log('☕️', order1);
+  const order2 = coffeeMachine1.makeCoffee({shots: 2, milk: true});
+  console.log('☕️', order2);
+  const order3 = coffeeMachine1.makeCoffee({shots: 3, milk: false});
+  console.log('☕️', order3);
+
+  const coffeeMachine2: CoffeeMaker = CoffeeMachine.create(80);
+  const order4 = coffeeMachine2.makeCoffee({shots: 2});
+  console.log('☕️', order4);
+
+  const coffeeMachine3: CommercialCoffeeMaker = CoffeeMachine.create(80);
+  coffeeMachine3.fillCoffeeBeans(100);
+  const order5 = coffeeMachine3.makeCoffee({shots: 3, milk: true});
+  console.log('☕️', order5);
 }
